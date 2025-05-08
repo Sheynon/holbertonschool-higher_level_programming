32c00e3e0635822535829792df3d08db851f7862
Sheynon <labadie.jonatahan@hotmail.fr>
2025-05-08 15:59:33 +0200

    added - print number from 0 to 99 with two digits

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,2 +1,6 @@
 #!/usr/bin/python3
-print(", ".join([f"{i:02}" for i in range(100)]))
+for i in range(100):
+    if i != 99:
+        print("{:02d}, ".format(i), end='')
+    else:
+        print("{:02d}".format(i))
