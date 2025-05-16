06d9c5acc45f9368c7e9178831aad519811678a4
Sheynon <labadie.jonatahan@hotmail.fr>
2025-05-16 16:12:08 +0200

    added - function to divide elements of a matrix

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -25,8 +25,8 @@ def matrix_divided(matrix, div):
 
     # Check if matrix is a list of lists of ints/floats
     if (not isinstance(matrix, list) or
-        not all(isinstance(row, list) for row in matrix) or
-        not all(isinstance(num, (int, float)) for row in matrix for num in row)):
+            not all(isinstance(row, list) for row in matrix) or
+            not all(isinstance(num, (int, float)) for row in matrix for num in row)):
         raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
 
     # Check if all rows are the same size
