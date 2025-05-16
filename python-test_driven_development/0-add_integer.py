003b21568897de350619d35185a4ee66f151d2b6
Sheynon <labadie.jonatahan@hotmail.fr>
2025-05-16 14:21:57 +0200

    added - use exemples

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -14,7 +14,17 @@ def add_integer(a, b=89):
         The sum of a and b as an interger.
 
     Raises:
-        TypeErrir: If a or b are not intergers or flats.
+        TypeError: If a or b are not intergers or flats.
+
+    Examples:
+    >>> add_integer(1, 2)
+    3
+    >>> add_integer(100, -2)
+    98
+    >>> add_integer(2)
+    100
+    >>> add_integer(100.3, -2)
+    98
     """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
