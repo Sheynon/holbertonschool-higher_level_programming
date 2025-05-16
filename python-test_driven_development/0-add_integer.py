fbce93c69e22b914b0d025c693c6a8a4ecba56f5
Sheynon <labadie.jonatahan@hotmail.fr>
2025-05-16 14:07:29 +0200

    added - function to add 2 integer and raise exception

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def add_integer(a, b=89):
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    result = a + b
+    return result
