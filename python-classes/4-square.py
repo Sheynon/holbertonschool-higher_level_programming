07beab71fe7ba3f3731c32be3afb2a8774b3c5e2
Sheynon <labadie.jonatahan@hotmail.fr>
2025-05-21 15:12:57 +0200

    added - propety setter and getter

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -5,15 +5,21 @@
 class Square:
     """Square class with private size attribute and area computation"""
     def __init__(self, size=0):
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
+        self.size = size
 
+    @property
     def size(self):
-        """return the size of the square"""
+        """Getter for size."""
         return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter for size with type and value validation."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
 
     def area(self):
         """return the current square area"""
