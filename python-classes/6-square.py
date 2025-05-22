d9f562f2fef1698ae10451bbe52d7b264a84ec5f
Sheynon <labadie.jonatahan@hotmail.fr>
2025-05-22 10:23:10 +0200

    fixed - identaton of condition

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -32,10 +32,12 @@ class Square:
     def position(self, value):
         """Setter for position with type validation."""
         if (not isinstance(value, tuple) or
-            len(value) != 2 or
-            not all(isinstance(num, int) for num in value) or
-            not all(num >= 0 for num in value)):
-            raise TypeError("position must be a tuple of 2 positive integers")
+                len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
+            raise TypeError(
+                "position must be a tuple of 2 positive integers"
+            )
         self.__position = value
 
     def area(self):
