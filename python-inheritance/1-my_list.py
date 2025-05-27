8567d920d288e350b9cbceaed1fe237758d17fcc
Sheynon <labadie.jonatahan@hotmail.fr>
2025-05-27 13:49:40 +0200

    added - method to print a sorted list in a inheritance

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""Class MyList with an inheritance for the class list"""
+
+
 class MyList(list):
+    """Class MyList taking inheritance from list"""
     def print_sorted(self):
+        """Method to print a sorted list"""
         sorted_list = sorted(self)
         print("{}".format(sorted_list))
