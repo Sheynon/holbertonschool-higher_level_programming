741edab003f3a19ae61c833cb7a3e05b9f40ca3e
Sheynon <labadie.jonatahan@hotmail.fr>
2025-05-27 13:47:53 +0200

    added - show all the attribute of a class

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
+"""Look up function"""
+
+
 def lookup(obj):
+    """ function that returns the list of available attributes
+    and methods of an object
+
+    Args:
+        obj: Object
+    """
     return dir(obj)
