481d204ccb094efde383a758bde2a40dc3116bc7
Sheynon <labadie.jonatahan@hotmail.fr>
2025-06-06 21:01:42 +0200

    added - module to create a class who can serialize and deserialize it's instance

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Class CustomObject with serialization and deserialization"""
+
+import pickle
+
+
+class CustomObject:
+    """CustomObject Class"""
+    def __init__(self, name, age, is_student):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Is Student: {self.is_student}")
+
+    def serialize(self, filename):
+        with open(filename, "wb") as f:
+            pickle.dump(self, f)
+
+    @classmethod
+    def deserialize(cls, filename):
+        with open(filename, "rb") as f:
+            return pickle.load(f)
