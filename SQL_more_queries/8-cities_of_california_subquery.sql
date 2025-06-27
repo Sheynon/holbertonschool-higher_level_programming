329ae019a0557d780c3b729932b9f13ef0c9c704
Sheynon <labadie.jonatahan@hotmail.fr>
2025-06-27 23:29:35 +0200

    added - all task

---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,7 @@
+-- 9 query cities in california
+SELECT id, name
+FROM cities
+WHERE state_id = (
+    SELECT id FROM states WHERE name = "California"
+)
+ORDER BY id ASC;
---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,5 @@
+-- 9 query cities by state using join
+SELECT cities.id, cities.name, states.name
+FROM cities
+INNER JOIN states ON cities.state_id = states.id
+ORDER BY cities.id ASC;
