63f2973125cbb6b8e6ff3450081825b9c705c1cc
Sheynon <labadie.jonatahan@hotmail.fr>
2025-07-18 23:15:43 +0200

    added - flask app that serves a web page using jinja template

--- python-server_side_rendering/task_01_jinja.py
+++ python-server_side_rendering/task_01_jinja.py
@@ -4,15 +4,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')  # Render the home page template
+    return render_template('index.html')
 
 @app.route('/about')
 def about():
-    return render_template('about.html')  # Render the about page template
+    return render_template('about.html')
 
 @app.route('/contact')
 def contact():
-    return render_template('contact.html')  # Render the contact page template
+    return render_template('contact.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
